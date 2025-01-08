b370c7a9d5950429d1393de7b6f47ee9c5a8e123
Sunke718 <142437871+Sunke718@users.noreply.github.com>
2025-01-08 14:35:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.sunzy.link": dockerHub,
+  "quay.sunzy.link": "https://quay.io",
+  "gcr.sunzy.link": "https://gcr.io",
+  "k8s-gcr.sunzy.link": "https://k8s.gcr.io",
+  "k8s.sunzy.link": "https://registry.k8s.io",
+  "ghcr.sunzy.link": "https://ghcr.io",
+  "cloudsmith.sunzy.link": "https://docker.cloudsmith.io",
+  "ecr.sunzy.link": "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+    "docker-staging.sunzy.link": dockerHub,
 };
 
 function routeByHosts(host) {
